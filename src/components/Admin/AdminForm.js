db6c8a7e6948dc09e795d24a00716207c01759e7
Britt Carter <brittbmh@gmail.com>
2019-02-03 15:58:25 -0600

    add some styling to admin new project form

--- src/components/Admin/AdminForm.js
+++ src/components/Admin/AdminForm.js
@@ -2,6 +2,35 @@ import React, { Component } from 'react';
 import Dropdown from './Dropdown.js';
 import DateInput from './DateInput.js';
 import { connect } from 'react-redux';
+//material-ui
+import Button from '@material-ui/core/Button';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+
+const styles = theme => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    textField: {
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
+        width: 200,
+    },
+    dense: {
+        marginTop: 19,
+    },
+    menu: {
+        width: 200,
+    },
+});
 
 class AdminForm extends Component {
     // create form to add new project to database
@@ -50,17 +79,39 @@ class AdminForm extends Component {
 
     // form to accept info for new project to add to database
     render() {
+        const { classes } = this.props;
+
         return (
             <div>
                 <br />
-                <form id="projectForm" onSubmit={this.sendProject}>
-                    <input placeholder="Project Name" required onChange={this.addInput('name')} />
-                    <input placeholder="Website URL (optional)" onChange={this.addInput('website')} />
-                    <input placeholder="GitHub URL" required onChange={this.addInput('github')} />
-                    <input placeholder="Description" required onChange={this.addInput('desc')} />
+                <form className={classes.root}  id="projectForm" autoComplete="off" onSubmit={this.sendProject}>
+                    <TextField 
+                        id="project-name"
+                        label="Project Name"
+                        className={classes.textField}
+                        margin="normal"
+                        placeholder="Name" required onChange={this.addInput('name')} />
+                   
+                    <TextField id="project-website"
+                        label="Project Website"
+                        className={classes.textField}
+                        margin="normal"
+                        placeholder="Website URL (optional)" onChange={this.addInput('website')} />
+                    
+                    <TextField id="project-github"
+                        label="Github Repository"
+                        className={classes.textField}
+                        margin="normal"
+                        placeholder="GitHub URL" required onChange={this.addInput('github')} />
+                    
+                    <TextField 
+                        id="project-desc"
+                        label="Project Description"
+                        className={classes.textField}
+                        margin="normal" placeholder="Description" required onChange={this.addInput('desc')} />
                     <DateInput setDate={this.handleDateChange} />
                     <Dropdown required setTag={this.handleSetTag} />
-                    <button type="submit">Submit</button>
+                    <Button type="submit">Submit</Button>
                 </form>
                 <br />
             </div>
@@ -68,4 +119,4 @@ class AdminForm extends Component {
     }
 }
 
-export default connect()(AdminForm);
+export default withStyles(styles)(connect()(AdminForm));
--- src/components/Admin/Dropdown.js
+++ src/components/Admin/Dropdown.js
@@ -1,5 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+//material-ui
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import Input from '@material-ui/core/Input';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import FilledInput from '@material-ui/core/FilledInput';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import FormControl from '@material-ui/core/FormControl';
+
+const styles = theme => ({
+    formControl: {
+        margin: theme.spacing.unit,
+        minWidth: 120,
+    },
+    selectEmpty: {
+        marginTop: theme.spacing.unit * 2,
+    },
+});
 
 
 class Dropdown extends Component {
@@ -13,22 +34,30 @@ class Dropdown extends Component {
     // function to pull tags from redux store to display in dropdown
     buildSelectInput = () => {
         const options = this.props.tags.map((tag, index) => {
-            return (<option key={index}
+            return (<MenuItem key={index}
                 value={tag.id}>
                 {tag.name}
-            </option>
+            </MenuItem>
             )
         })
         return options;
     }
 
     render() {
+        const { classes } = this.props;
+
         return (
             <div>
-                    <select defaultValue='' required onChange={this.handleChange}>
-                        <option value="" disabled defaultValue>Select a Tag</option>
+                <FormControl className="{class.formControl}">
+                    <Select defaultValue='' required onChange={this.handleChange}
+                        inputProps={{
+                            name: 'tag',
+                            id: 'project-tag',
+                        }}>
+                        <MenuItem value="" disabled defaultValue>Tag</MenuItem>
                         {this.buildSelectInput()}
-                    </select>
+                    </Select>
+                </FormControl>
             </div>
         )
     }
@@ -38,4 +67,4 @@ const mapReduxStoreToProps = reduxStore => ({
     tags: reduxStore.tags
 })
 
-export default connect(mapReduxStoreToProps)(Dropdown);
+export default withStyles(styles)(connect(mapReduxStoreToProps)(Dropdown));
